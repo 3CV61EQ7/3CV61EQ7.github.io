bd469107f7ebbc9b0d98d3d794e158045e4de7e2
3CV61EQ7 <95831908+3CV61EQ7@users.noreply.github.com>
2021-12-09 00:30:44 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+firebase.initializeApp({
+  apiKey: "AIzaSyBCGkLiCCz0AmFkAgsJH8RX2p6j9Y9Jizc",
+  authDomain: "ubicua-eb1c6.firebaseapp.com",
+  projectId: "ubicua-eb1c6",
+  storageBucket: "ubicua-eb1c6.appspot.com",
+  messagingSenderId: "41113952430",
+  appId: "1:41113952430:web:7b3a582e0d661a3bb74d3f",
+  measurementId: "${config.measurementId}"
+});
+
+
+
