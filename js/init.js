37370868749d915dff68c6e1c229c74a6f0d222c
3CV61EQ7 <95831908+3CV61EQ7@users.noreply.github.com>
2021-12-08 18:42:52 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyAknrvw3brgo7qxEyMk3jzsE02CyJKWo5c",
-  authDomain: "fbauten.firebaseapp.com",
-  projectId: "fbauten",
-  storageBucket: "fbauten.appspot.com",
-  messagingSenderId: "902838344264",
-  appId: "1:902838344264:web:221d59cd0ba838db98567d",
-  measurementId: "G-52E0LME3E0"
+   apiKey: "AIzaSyBCGkLiCCz0AmFkAgsJH8RX2p6j9Y9Jizc",
+  authDomain: "ubicua-eb1c6.firebaseapp.com",
+  projectId: "ubicua-eb1c6",
+  storageBucket: "ubicua-eb1c6.appspot.com",
+  messagingSenderId: "41113952430",
+  appId: "1:41113952430:web:7b3a582e0d661a3bb74d3f",
+  measurementId: "${config.measurementId}"
 });
